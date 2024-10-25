6904a4e117fad6140e2c98fedaaa1175fd499b90
DuNguyen107 <dule1028a@gmail.com>
2024-10-25 22:35:21 +0700

    fix, update form địa chỉ

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const adressbtn = document.querySelector('#address-form');
 const adressclose = document.querySelector('#address-close');
 adressbtn.addEventListener("click", function () {//them 1 su kien
     document.querySelector('.address-form').style.display = 'flex';
+    document.body.style.overflow = 'hidden';
 })
 adressclose.addEventListener("click", function () {//them 1 su kien
     document.querySelector('.address-form').style.display = 'none';
+    document.body.style.overflow = 'auto';
 })
 // banner
 document.querySelector('.big-banner .ti-close').addEventListener('click', function () {
