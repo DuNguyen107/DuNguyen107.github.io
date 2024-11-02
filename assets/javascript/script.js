e1211ce69c96b5b1625c0ffa1b0b4088dbb5becf
DuNguyen107 <dule1028a@gmail.com>
2024-11-02 18:22:55 +0700

    thêm trang

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 const adressbtn = document.querySelector('#address-form');
 const adressclose = document.querySelector('#address-close');
-adressbtn.addEventListener("click", function () {//them 1 su kien
+adressbtn.addEventListener("click", function () { //them 1 su kien
     document.querySelector('.address-form').style.display = 'flex';
     document.body.style.overflow = 'hidden';
 })
